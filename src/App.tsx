2bf576c25a34ea8405506445003915540573f0c7
Oscar Vargas <oscar.andres.vargas1@gmail.com>
2023-08-03 23:25:38 +0200

    Restoring users

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import { UsersList } from './components/UsersList/UsersList';
 import { User } from './components/UsersList/User.interface';
@@ -7,20 +7,26 @@ import { ButtonOptions } from './components/ButtonOptions/ButtonOptions';
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [paintedRows, setPaintedRows] = useState<boolean>(false);
+  const initialValues = useRef<User[]>([]);
 
   useEffect(() => {
     fetch('https://randomuser.me/api/?results=100')
     .then(async res => res.json())    
     .then(res => {
       setUsers(res.results);
+      initialValues.current = res.results;
     })
     .catch( e => console.log(e));
   },[]);
 
+  const restoreUsers = () => {
+    setUsers(initialValues.current);
+  };
+
   return (
     <div className='App' style={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
       <h1>Table with candidates</h1>
-      <ButtonOptions paintedRows={paintedRows} setPaintedRows={setPaintedRows}/>
+      <ButtonOptions paintedRows={paintedRows} setPaintedRows={setPaintedRows} restoreUsers={restoreUsers}/>
       <UsersList users={users} setUsers={setUsers} paintedRows={paintedRows}/>
     </div>
   )
--- src/components/ButtonOptions/ButtonOptions.tsx
+++ src/components/ButtonOptions/ButtonOptions.tsx
@@ -1,6 +1,7 @@
 type ButtonOptionsProps = {
     paintedRows: boolean,
     setPaintedRows: React.Dispatch<React.SetStateAction<boolean>>,
+    restoreUsers: () => void,
 }
 
 export function ButtonOptions (props: ButtonOptionsProps) {
@@ -8,7 +9,7 @@ export function ButtonOptions (props: ButtonOptionsProps) {
         <div style={{ display: 'flex', flexDirection: 'row', gap: '15px', justifyContent: 'center', alignContent: 'center' }}>
             <button onClick={() => props.setPaintedRows(!props.paintedRows)}>Paint Rows</button>
             <button>Sort By Country</button>
-            <button>Resotre Initial State</button>
+            <button onClick={() => props.restoreUsers()}>Resotre Initial State</button>
             <input type="text" placeholder="Filter By Country" />
         </div>
     )

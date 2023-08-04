d1d1dcd77ade1f9948c7d6d7e2ada7d7b7f0cac4
Oscar Vargas <oscar.andres.vargas1@gmail.com>
2023-08-04 21:16:25 +0200

    Filtering by Country

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
     const [users, setUsers] = useState<User[]>([]);
     const [paintedRows, setPaintedRows] = useState<boolean>(false);
     const [sortByCountryEnabled, setSortByCountryEnabled] = useState<boolean>(false);
+    const [filterByCountry, setFilterByCountry] = useState<string>('');
     const initialValues = useRef<User[]>([]);
 
     useEffect(() => {
@@ -24,20 +25,22 @@ function App() {
         setUsers(initialValues.current);
     };
 
-    const usersSortedByCountry = () => {
-        return [...users].sort((a, b) => {
+    const usersFilteredByCountry = filterByCountry
+    ? users.filter(u => u.location.country.toLocaleLowerCase().includes(filterByCountry.toLocaleLowerCase()))
+    : users;
+
+    const sortedUsers = sortByCountryEnabled 
+        ?  [...usersFilteredByCountry].sort((a, b) => {
             const userA = a.location.country;
             const userB = b.location.country;
             return userA.localeCompare(userB);
-        });
-    }
-
-    const sortedUsers = sortByCountryEnabled ? usersSortedByCountry() : users;
+        })
+        : usersFilteredByCountry;
 
     return (
         <div className='App' style={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
             <h1>Table with candidates</h1>
-            <ButtonOptions paintedRows={paintedRows} setPaintedRows={setPaintedRows} restoreUsers={restoreUsers} sortByCountryEnabled={sortByCountryEnabled} setSortByCountryEnabled={setSortByCountryEnabled} />
+            <ButtonOptions paintedRows={paintedRows} setPaintedRows={setPaintedRows} restoreUsers={restoreUsers} sortByCountryEnabled={sortByCountryEnabled} setSortByCountryEnabled={setSortByCountryEnabled} setFilterByCountry={setFilterByCountry} />
             <UsersList users={sortedUsers} setUsers={setUsers} paintedRows={paintedRows} />
         </div>
     )
--- src/components/ButtonOptions/ButtonOptions.tsx
+++ src/components/ButtonOptions/ButtonOptions.tsx
@@ -4,15 +4,21 @@ type ButtonOptionsProps = {
     restoreUsers: () => void,
     sortByCountryEnabled: boolean,
     setSortByCountryEnabled: React.Dispatch<React.SetStateAction<boolean>>,
+    setFilterByCountry: React.Dispatch<React.SetStateAction<string>>,
 }
 
 export function ButtonOptions(props: ButtonOptionsProps) {
+    const setFilterByCountry = props.setFilterByCountry;
+    const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFilterByCountry(e.target.value);
+    }
+
     return (
         <div style={{ display: 'flex', flexDirection: 'row', gap: '15px', justifyContent: 'center', alignContent: 'center' }}>
             <button onClick={() => props.setPaintedRows(!props.paintedRows)}>Paint Rows</button>
             <button onClick={() => props.setSortByCountryEnabled(prevState => !prevState)}>{props.sortByCountryEnabled ? 'Do not Sort By Country' : 'Sort By Country'}</button>
             <button onClick={() => props.restoreUsers()}>Resotre Initial State</button>
-            <input type="text" placeholder="Filter By Country" />
+            <input type="text" placeholder="Filter By Country" onChange={inputHandler}/>
         </div>
     )
 

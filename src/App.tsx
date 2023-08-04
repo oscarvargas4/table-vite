e9ef4845bb7b68f25dbfb3052495054c1c1b72a8
Oscar Vargas <oscar.andres.vargas1@gmail.com>
2023-08-04 19:29:23 +0200

    Sort by  country

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,42 @@ import { User } from './components/UsersList/User.interface';
 import { ButtonOptions } from './components/ButtonOptions/ButtonOptions';
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [paintedRows, setPaintedRows] = useState<boolean>(false);
-  const initialValues = useRef<User[]>([]);
+    const [users, setUsers] = useState<User[]>([]);
+    const [paintedRows, setPaintedRows] = useState<boolean>(false);
+    const [sortByCountryEnabled, setSortByCountryEnabled] = useState<boolean>(false);
+    const initialValues = useRef<User[]>([]);
 
-  useEffect(() => {
-    fetch('https://randomuser.me/api/?results=100')
-    .then(async res => res.json())    
-    .then(res => {
-      setUsers(res.results);
-      initialValues.current = res.results;
-    })
-    .catch( e => console.log(e));
-  },[]);
+    useEffect(() => {
+        fetch('https://randomuser.me/api/?results=100')
+            .then(async res => res.json())
+            .then(res => {
+                setUsers(res.results);
+                initialValues.current = res.results;
+            })
+            .catch(e => console.log(e));
+    }, []);
 
-  const restoreUsers = () => {
-    setUsers(initialValues.current);
-  };
+    const restoreUsers = () => {
+        setUsers(initialValues.current);
+    };
 
-  return (
-    <div className='App' style={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
-      <h1>Table with candidates</h1>
-      <ButtonOptions paintedRows={paintedRows} setPaintedRows={setPaintedRows} restoreUsers={restoreUsers}/>
-      <UsersList users={users} setUsers={setUsers} paintedRows={paintedRows}/>
-    </div>
-  )
+    const usersSortedByCountry = () => {
+        return [...users].sort((a, b) => {
+            const userA = a.location.country;
+            const userB = b.location.country;
+            return userA.localeCompare(userB);
+        });
+    }
+
+    const sortedUsers = sortByCountryEnabled ? usersSortedByCountry() : users;
+
+    return (
+        <div className='App' style={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
+            <h1>Table with candidates</h1>
+            <ButtonOptions paintedRows={paintedRows} setPaintedRows={setPaintedRows} restoreUsers={restoreUsers} sortByCountryEnabled={sortByCountryEnabled} setSortByCountryEnabled={setSortByCountryEnabled} />
+            <UsersList users={sortedUsers} setUsers={setUsers} paintedRows={paintedRows} />
+        </div>
+    )
 }
 
 export default App;
--- src/components/ButtonOptions/ButtonOptions.tsx
+++ src/components/ButtonOptions/ButtonOptions.tsx
@@ -2,13 +2,15 @@ type ButtonOptionsProps = {
     paintedRows: boolean,
     setPaintedRows: React.Dispatch<React.SetStateAction<boolean>>,
     restoreUsers: () => void,
+    sortByCountryEnabled: boolean,
+    setSortByCountryEnabled: React.Dispatch<React.SetStateAction<boolean>>,
 }
 
-export function ButtonOptions (props: ButtonOptionsProps) {
+export function ButtonOptions(props: ButtonOptionsProps) {
     return (
         <div style={{ display: 'flex', flexDirection: 'row', gap: '15px', justifyContent: 'center', alignContent: 'center' }}>
             <button onClick={() => props.setPaintedRows(!props.paintedRows)}>Paint Rows</button>
-            <button>Sort By Country</button>
+            <button onClick={() => props.setSortByCountryEnabled(prevState => !prevState)}>{props.sortByCountryEnabled ? 'Do not Sort By Country' : 'Sort By Country'}</button>
             <button onClick={() => props.restoreUsers()}>Resotre Initial State</button>
             <input type="text" placeholder="Filter By Country" />
         </div>
